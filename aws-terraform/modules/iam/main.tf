e570bf5d31fec5912d7b0bf3eb39848d0bba6d41
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2025-12-27 20:04:43 +0530

    test with changes

--- aws-terraform/modules/iam/main.tf
+++ aws-terraform/modules/iam/main.tf
@@ -16,8 +16,11 @@ resource "aws_iam_role" "github_actions" {
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:rockonmahi/microservice:*"
+          "token.actions.githubusercontent.com:sub" = "repo:rockonmahi/microservice:ref:refs/heads/main"
         }
       }
     }]
