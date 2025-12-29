15433c1c4d6b1f77d0fdcf96f9b605788649bbda
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2025-12-30 00:54:53 +0530

    initial test

--- aws-post-terraform/modules/security/main.tf
+++ aws-post-terraform/modules/security/main.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-alb-sg"
     Environment = var.project_name
@@ -22,10 +29,17 @@ resource "aws_security_group" "ecs" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port       = 8080
-    to_port         = 8080
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
--- aws-pre-terraform/modules/security/main.tf
+++ aws-pre-terraform/modules/security/main.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-alb-sg"
     Environment = var.project_name
@@ -26,6 +33,13 @@ resource "aws_security_group" "ecs" {
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
