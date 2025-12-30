76b93748e08484fd78a9e0c055e1bbfb3bfef60e
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2025-12-30 22:10:57 +0530

    code structure

--- aws-post-terraform/modules/ecs/main.tf
+++ aws-post-terraform/modules/ecs/main.tf
@@ -53,7 +53,7 @@ resource "aws_ecs_service" "ecs_service_web_server" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [var.public_subnets]
+    subnets         = [var.private_subnets,var.public_subnets]
     security_groups = [var.ecs_sg_id]
     /* assign_public_ip = true*/
   }
