9af1c0344081adbe7ab82ea337c2d0625651cd28
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-15 23:09:08 +0530

    s3 path changes

--- aws-post-terraform/backend.tf
+++ aws-post-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "dev-microservice-terraform-state"
-    key     = "eks/terraform.tfstate"
+    key     = "terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
