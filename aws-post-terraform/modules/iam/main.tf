16355dae56ea0e298e19719b83b6530ab903592a
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-15 22:10:27 +0530

    environment dev

--- aws-post-terraform/modules/iam/main.tf
+++ aws-post-terraform/modules/iam/main.tf
@@ -1,9 +1,10 @@
 resource "aws_iam_openid_connect_provider" "github" {
+  name = "${var.project_name}-github"
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
 
   tags = {
-    Name        = "${var.project_name}-openid-connect-provider"
+    Name        = "${var.project_name}-github"
     Environment = var.project_name
   }
 }
@@ -36,23 +37,33 @@ resource "aws_iam_role" "github_actions" {
   })
 
   tags = {
-    Name        = "${var.project_name}-iam-role"
+    Name        = "${var.project_name}-github-actions"
     Environment = var.project_name
   }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_full_access_github" {
+  name = "${var.project_name}-ecs-full-access-github"
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+  tags = {
+    Name        = "${var.project_name}-ecs-full-access-github"
+    Environment = var.project_name
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "admin_access_github" {
+  name = "${var.project_name}-admin-access-github"
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  tags = {
+    Name        = "${var.project_name}-admin-access-github"
+    Environment = var.project_name
+  }
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecsExecutionRole"
+  name = "${var.project_name}-ecs-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -64,58 +75,27 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 
   tags = {
-    Name        = "${var.project_name}-ecs-iam-role"
+    Name        = "${var.project_name}-ecs-execution-role"
     Environment = var.project_name
   }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  tags = {
+      Name        = "${var.project_name}-ecs-task-execution-role"
+      Environment = var.project_name
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_container_registry_read" {
+  name = "${var.project_name}-ec2-container-registry-read"
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  tags = {
+        Name        = "${var.project_name}-ec2-container-registry-read"
+        Environment = var.project_name
+  }
 }
