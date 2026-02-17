bd24bc29ec52547891547c80671e5fccb3a2b6ea
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-17 23:18:34 +0530

    auth environment deployment

--- aws-pre-terraform/modules/alb/main.tf
+++ aws-pre-terraform/modules/alb/main.tf
@@ -135,7 +135,7 @@ resource "aws_lb_target_group" "api_gateway_alb_target_group" {
   }
 }
 
-resource "aws_lb_target_group" "auth_server_alb_target_group" {
+resource "aws_lb_target_group" "authentication_server_alb_target_group" {
   name        = "${var.project_name}-alb-tg-auth-server"
   port        = var.authentication_server_port
   protocol    = "HTTP"
@@ -246,7 +246,7 @@ resource "aws_lb_listener_rule" "authentication_server_listener_rule" {
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.auth_server_alb_target_group.arn
+    target_group_arn = aws_lb_target_group.authentication_server_alb_target_group.arn
   }
 
   condition {
