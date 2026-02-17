b10e41f203d012161d32407a1f1d6d3718d32b89
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-17 23:50:46 +0530

    environment chnages

--- aws-post-terraform/backend.tf
+++ aws-post-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "microservice-terraform-state"
+    #bucket  = "microservice-terraform-state"
     key     = "terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
