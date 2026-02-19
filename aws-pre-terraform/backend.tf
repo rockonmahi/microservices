707eafc109e4c68005145792bffc04cef05dfc09
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-19 15:00:05 +0530

    new s3 bucket

--- aws-pre-terraform/backend.tf
+++ aws-pre-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    #bucket  = "dev-microservice-terraform-state"
+    bucket  = "dev-microservice-terraform-state"
     key     = "terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
