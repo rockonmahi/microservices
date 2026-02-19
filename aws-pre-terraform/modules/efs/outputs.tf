c7a93513517fd2a538c50c5066a1321ebac237de
Mahendra Singh <mahendra.singh.ext@zeiss.com>
2026-02-19 15:01:46 +0530

    efs

---
+++ aws-pre-terraform/modules/efs/outputs.tf
@@ -0,0 +1,3 @@
+output "mongo_db_efs_file_system_id" {
+  value = aws_efs_file_system.mongo_db_efs_file_system.id
+}
---
+++ aws-pre-terraform/modules/efs/variables.tf
@@ -0,0 +1,8 @@
+variable "project_name" {
+}
+
+variable "private_subnets" {
+}
+
+variable "database_sg_id" {
+}
